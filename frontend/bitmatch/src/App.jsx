9491ee3bd8d16ec937412163e5b947e58873b518
Rebecca Smith <2145912+Rebeccals@users.noreply.github.com>
2025-02-13 22:22:06 -0800

    Added API path for A3 (#3)
    * Added API path for A3
    
    * chore: remove comments
    
    ---------
    
    Co-authored-by: Larry La <larryquocla@gmail.com>

--- frontend/bitmatch/src/App.jsx
+++ frontend/bitmatch/src/App.jsx
@@ -6,6 +6,9 @@ function App() {
     <>
       <h1 className="text-5xl text-center">Hello World!</h1>
       <Button>Click Me</Button>
+      <br /><br />
+      <h1 className="text-5xl text-center">Rebecca Smith</h1>
+      <a href="">This is a link to something</a>
     </>
   );
 }
