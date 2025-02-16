aa6b8babaeae950bd586b24cd3639353610e77d6
ColumnSkunky <wfgs03@gmail.com>
2025-02-15 19:28:07 -0800

    feat: william route

--- backend/bitmatch/a3/urls.py
+++ backend/bitmatch/a3/urls.py
@@ -5,9 +5,8 @@ urlpatterns = [
     path('', views.base_api, name='base_api'),
     path('larry', views.simple_api_larry, name='simple_api_larry'),
     path('rebecca', views.simple_api_rebecca, name='simple_api_rebecca'),
-    path('luis', views.simple_api_luis, name='simple_api_luis')
+    path('luis', views.simple_api_luis, name='simple_api_luis'),
+    path('william', views.simple_api_william, name='sample_api_william'),
 
-
-    # TODO: ADD WILLIAM API PATH
 ]
 
