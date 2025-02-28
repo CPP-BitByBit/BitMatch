8d0b46b157456a082d2a9bcac0ab885eed9b4707
Rebecca Smith <2145912+Rebeccals@users.noreply.github.com>
2025-02-28 08:50:53 -0800

    Adding ImageKit Library to the build for A4

--- backend/bitmatch/a4/urls.py
+++ backend/bitmatch/a4/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('gemini-test/', views.gemini_test_GET, name='gemini_test_GET'),
+    path('upload-image/', views.upload_image, name='upload_image'),
 ]
