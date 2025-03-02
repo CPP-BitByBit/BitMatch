440deb16719ac77cc7274f531e90eda506dcbd0b
Larry La <larryquocla@gmail.com>
2025-03-01 19:35:15 -0800

    chore: resolve merge conflict

--- backend/bitmatch/a4/views.py
+++ backend/bitmatch/a4/views.py
@@ -5,7 +5,11 @@ from django.http import JsonResponse
 import os
 load_dotenv()
 from google import genai
+from django.views.decorators.csrf import csrf_exempt
+from .models import UserProfile
+import matplotlib.pyplot as plt
 import numpy as np
+
 # Create your views here.
 
 # Larry La - A4
@@ -35,3 +39,29 @@ def numpy_test_GET(request):
             return JsonResponse({"error": str(e)}, status=500)
     else:
         return JsonResponse({"error": "Only GET requests are allowed"}, status=405)
+    
+# Rebecca Smith - django-imagekit - A4
+# ImageKit is a Django app for processing images.
+@csrf_exempt
+def upload_image(request):
+    if request.method == "POST" and request.FILES.get("image"):
+        image = request.FILES["image"]
+        user_profile = UserProfile(image=image)
+        user_profile.save()
+        return JsonResponse({"message": "Image uploaded successfully!"}, status=201)
+
+    return JsonResponse({"error": "Invalid request"}, status=400)
+
+# William Garica - A4 
+def matplotlib_test_GET(request):
+    if request.method == "GET":
+        try:
+           x = [1,2,3,4]
+           e = (0.1, 0, 0.1, 0)
+           plt.pie(x, explode = e)
+           plt.title("Example")
+           plt.show()
+        except Exception as e:
+            return JsonResponse({"error":str(e)},status = 500)
+    else:
+        return JsonResponse({"error":"Only GET requests are allowed"},status = 405)
