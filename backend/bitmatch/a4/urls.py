d3eb525f8839dc3c32301e3a0796f6bd31c36b74
ColumnSkunky <wfgs03@gmail.com>
2025-03-01 16:08:33 -0800

    added William Garcia A4 libaries

--- backend/bitmatch/a4/urls.py
+++ backend/bitmatch/a4/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('gemini-test/', views.gemini_test_GET, name='gemini_test_GET'),
+    path('matplotlib-test/', views.matplotlib_test_GET, name = 'matplotlib_test_GET'),
 ]
--- backend/bitmatch/a4/views.py
+++ backend/bitmatch/a4/views.py
@@ -5,7 +5,7 @@ from django.http import JsonResponse
 import os
 load_dotenv()
 from google import genai
-
+import matplotlib.pyplot as plt
 # Create your views here.
 
 # Larry La - A4
@@ -26,3 +26,16 @@ def gemini_test_GET(request):
         return JsonResponse({"error": "Only GET requests are allowed"}, status=405)
 
 
+# William Garica - A4 
+def matplotlib_test_GET(request):
+    if request.method == "GET":
+        try:
+           x = [1,2,3,4]
+           e = (0.1, 0, 0.1, 0)
+           plt.pie(x, explode = e)
+           plt.title("Example")
+           plt.show()
+        except Exception as e:
+            return JsonResponse({"error":str(e)} status = 500)
+    else:
+        return JsonResponse({"error":"Only GET requests are allowed"},status = 405)
