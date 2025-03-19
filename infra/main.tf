cafdec78291c9473cfd50f97236b04cbecfea641
Larry La <larryquocla@gmail.com>
2025-03-19 10:22:02 -0700

    feat: frontend infra

--- infra/main.tf
+++ infra/main.tf
@@ -265,5 +265,117 @@ resource "aws_iam_access_key" "django_s3_user_key" {
 }
 
 
-####### FRONTEND RESOURCES
-# TODO: PROVISION S3 WITH DEPLOY, CLOUDFRONT, ROUTE 53
+# S3 Bucket for React App
+resource "aws_s3_bucket" "react_frontend" {
+  bucket = "bitmatch-frontend-bucket"
+}
+
+# S3 Bucket Public Access Block
+resource "aws_s3_bucket_public_access_block" "frontend_access_block" {
+  bucket = aws_s3_bucket.react_frontend.id
+
+  block_public_acls   = true
+  block_public_policy = true
+  ignore_public_acls  = true
+  restrict_public_buckets = true
+}
+
+# S3 Bucket Policy for CloudFront OAC Access
+resource "aws_s3_bucket_policy" "frontend_policy" {
+  bucket = aws_s3_bucket.react_frontend.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Sid       = "AllowCloudFrontAccessOAC"
+      Effect    = "Allow"
+      Principal = { Service = "cloudfront.amazonaws.com" }
+      Action    = "s3:GetObject"
+      Resource  = "arn:aws:s3:::bitmatch-frontend-bucket/*"
+      Condition = {
+        StringEquals = {
+          "AWS:SourceArn" = aws_cloudfront_distribution.react_cdn.arn
+        }
+      }
+    }]
+  })
+}
+
+# S3 Bucket Website Configuration
+resource "aws_s3_bucket_website_configuration" "frontend_website" {
+  bucket = aws_s3_bucket.react_frontend.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "index.html"
+  }
+}
+
+# CloudFront Origin Access Control (OAC)
+resource "aws_cloudfront_origin_access_control" "frontend_oac" {
+  name                              = "BitMatchReactOAC"
+  description                       = "OAC for React Frontend"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+# CloudFront Distribution for React App with OAC
+resource "aws_cloudfront_distribution" "react_cdn" {
+  origin {
+    domain_name              = aws_s3_bucket.react_frontend.bucket_regional_domain_name
+    origin_id                = "S3ReactOrigin"
+    origin_access_control_id = aws_cloudfront_origin_access_control.frontend_oac.id
+  }
+
+  enabled             = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "S3ReactOrigin"
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 86400
+    max_ttl                = 31536000
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  # Error Pages for React Router (Redirect everything to index.html)
+  custom_error_response {
+    error_code         = 403
+    response_code      = 200
+    response_page_path = "/index.html"
+  }
+
+  custom_error_response {
+    error_code         = 404
+    response_code      = 200
+    response_page_path = "/index.html"
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  tags = {
+    Name = "BitMatchReactCDN"
+  }
+}
