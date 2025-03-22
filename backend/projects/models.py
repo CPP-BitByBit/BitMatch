3e1e1fa056db725e6ee5069ce44c9ad54fe1c54c
Larry La <137100338+larrylaa@users.noreply.github.com>
2025-03-21 20:33:45 -0700

    feat: likes and follows routes (#28)
    * feat: like and follows route
    
    * fix: like and follow route
    
    ---------
    
    Co-authored-by: ColumnSkunky <wfgs03@gmail.com>

--- backend/projects/models.py
+++ backend/projects/models.py
@@ -1,18 +1,38 @@
 from django.db import models
+from userauth.models import User
 
-# TODO: PROJECTS MODEL
-# Create your models here.
+# PROJECTS MODEL
 class Project(models.Model):
     group = models.CharField(max_length=255)
     match_percentage = models.FloatField()
     title = models.CharField(max_length=255)
     institution = models.CharField(max_length=255)
     description = models.TextField()
-    followers = models.IntegerField(default=0)
-    likes = models.IntegerField(default=0)
+    followers_count = models.IntegerField(default=0) 
+    likes_count = models.IntegerField(default=0)  
     positions = models.JSONField(default=list)  
     image_url = models.ImageField(upload_to="projects/", blank=True, null=True)
     images = models.JSONField(default=list)
 
     def __str__(self):
         return self.title
+
+class Like(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name="project_likes")  
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_likes")  
+
+    class Meta:
+        unique_together = ('project', 'user')
+
+    def __str__(self):
+        return f"{self.project.title} received a like from {self.user.username}"
+
+class Follow(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='project_follows')  
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_follows")  
+
+    class Meta:
+        unique_together = ('project', 'user')
+
+    def __str__(self):
+        return f"{self.project.title} received a follow from {self.user.username}"
--- backend/userauth/admin.py
+++ backend/userauth/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import User  
 
-# Register your models here.
+# Register the User model
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'institution', 'followers', 'likes')
+    search_fields = ('username', 'email', 'institution')  
+    list_filter = ('institution',) 
+    ordering = ('username',)  
