f858c79eb7b0056a01bc7fdc3f7f893a04e09b99
Larry La <larryquocla@gmail.com>
2025-03-22 20:38:25 -0700

    fix: unit test model too long

--- backend/projects/tests.py
+++ backend/projects/tests.py
@@ -15,8 +15,8 @@ class ProjectModelTest(TestCase):
             title="Icebreak",
             institution="Cal Poly Pomona",
             description="We help connect organizations with their members.",
-            followers=10,
-            likes=5,
+            followers_count=10,
+            likes_count=5,
             positions=[{"role": "Backend Developer", "available": 2}],
             image_url="https://example.com/image.jpg"
         )
@@ -27,8 +27,8 @@ class ProjectModelTest(TestCase):
             title="Enigma Machine Emulator",
             institution="University of California, Irvine",
             description="An Enigma machine emulator that replicates the encryption process of the Enigma M3 series used by the Germans.",
-            followers=320,
-            likes=100,
+            followers_count=320,
+            likes_count=100,
             positions=[{"role": "Cyber Hacker", "available": 1}],
             image_url="https://www.101computing.net/wp/wp-content/uploads/enigma.png"
         )
@@ -38,11 +38,11 @@ class ProjectModelTest(TestCase):
             match_percentage=60,
             title="PersonalityPlaylist",
             institution="University of California, Riverside",
-            description="An app to create custom Spotify playlists based on a personality profile and classifying songs based on mood, tempo, etc",
-            followers=120,
-            likes=80,
+            description="An app to create custom Spotify playlists based on a personality profile.",
+            followers_count=120,
+            likes_count=80,
             positions=[{"role": "Backend Developer", "available": 2}],
-            image_url="https://media.wired.com/photos/633b6824f9c0de49c193489f/master/w_2240,c_limit/streamers-playlist-music-industry.jpg"
+            image_url="https://media.wired.com"
         )
         #William's test project 
         self.project4 = Project.objects.create(
@@ -51,8 +51,8 @@ class ProjectModelTest(TestCase):
             title="F1 Race Machine Learning Model",
             institution="University of California, Berkley",
             description="A machine learning model that takes data from F1 races and predicts winners based on previous results.",
-            followers=20,
-            likes=10,
+            followers_count=20,
+            likes_count=10,
             positions=[{"role": "Data Analyst", "available": 1}],
             image_url="https://robbreport.com/wp-content/uploads/2024/02/RR_2024_F1_Car_Roundup_Red_Bull_RB20.jpg"
         )
@@ -63,8 +63,8 @@ class ProjectModelTest(TestCase):
         self.assertEqual(self.project.title, "Icebreak")
         self.assertEqual(self.project.institution, "Cal Poly Pomona")
         self.assertEqual(self.project.description, "We help connect organizations with their members.")
-        self.assertEqual(self.project.followers, 10)
-        self.assertEqual(self.project.likes, 5)
+        self.assertEqual(self.project.followers_count, 10)
+        self.assertEqual(self.project.likes_count, 5)
         self.assertEqual(self.project.positions, [{"role": "Backend Developer", "available": 2}])
         self.assertEqual(self.project.image_url, "https://example.com/image.jpg")
 
@@ -75,8 +75,8 @@ class ProjectModelTest(TestCase):
         self.assertEqual(self.project2.title, "Enigma Machine Emulator")
         self.assertEqual(self.project2.institution, "University of California, Irvine")
         self.assertEqual(self.project2.description, "An Enigma machine emulator that replicates the encryption process of the Enigma M3 series used by the Germans.")
-        self.assertEqual(self.project2.followers, 320)
-        self.assertEqual(self.project2.likes, 100)
+        self.assertEqual(self.project2.followers_count, 320)
+        self.assertEqual(self.project2.likes_count, 100)
         self.assertEqual(self.project2.positions, [{"role": "Cyber Hacker", "available": 1}])
         self.assertEqual(self.project2.image_url, "https://www.101computing.net/wp/wp-content/uploads/enigma.png")
     
@@ -87,11 +87,11 @@ class ProjectModelTest(TestCase):
         self.assertEqual(self.project3.match_percentage, 60)
         self.assertEqual(self.project3.title, "PersonalityPlaylist")
         self.assertEqual(self.project3.institution, "University of California, Riverside")
-        self.assertEqual(self.project3.description, "An app to create custom Spotify playlists based on a personality profile and classifying songs based on mood, tempo, etc")
-        self.assertEqual(self.project3.followers, 120)
-        self.assertEqual(self.project3.likes, 80)
+        self.assertEqual(self.project3.description, "An app to create custom Spotify playlists based on a personality profile.")
+        self.assertEqual(self.project3.followers_count, 120)
+        self.assertEqual(self.project3.likes_count, 80)
         self.assertEqual(self.project3.positions, [{"role": "Backend Developer", "available": 2}])
-        self.assertEqual(self.project3.image_url, "https://media.wired.com/photos/633b6824f9c0de49c193489f/master/w_2240,c_limit/streamers-playlist-music-industry.jpg")
+        self.assertEqual(self.project3.image_url, "https://media.wired.com")
 
     # William's test project
     def test_project_creation_4(self): 
@@ -101,8 +101,8 @@ class ProjectModelTest(TestCase):
         self.assertEqual(self.project4.title, "F1 Race Machine Learning Model")
         self.assertEqual(self.project4.institution, "University of California, Berkley")
         self.assertEqual(self.project4.description, "A machine learning model that takes data from F1 races and predicts winners based on previous results.")
-        self.assertEqual(self.project4.followers, 20)
-        self.assertEqual(self.project4.likes, 10)
+        self.assertEqual(self.project4.followers_count, 20)
+        self.assertEqual(self.project4.likes_count, 10)
         self.assertEqual(self.project4.positions, [{"role": "Data Analyst", "available": 1}])
         self.assertEqual(self.project4.image_url, "https://robbreport.com/wp-content/uploads/2024/02/RR_2024_F1_Car_Roundup_Red_Bull_RB20.jpg")
 
