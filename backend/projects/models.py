c7c69bf46d9df4d6718c73a6a0c84948e1adebdb
ColumnSkunky <wfgs03@gmail.com>
2025-03-24 21:39:25 -0700

    feat: uuid implementation, email, full description, updates, wanted and links between user and project

--- backend/projects/models.py
+++ backend/projects/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from userauth.models import User
 from django.contrib.postgres.fields import ArrayField
-
+import uuid
 # PROJECTS MODEL
 class Project(models.Model):
+    project_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)                                                          
     group = models.CharField(max_length=255)
     match_percentage = models.FloatField()
     title = models.CharField(max_length=255)
@@ -15,6 +16,27 @@ class Project(models.Model):
     image_url = models.ImageField(upload_to="projects/", blank=True, null=True)
     images = models.JSONField(default=list)
     tags = ArrayField(models.CharField(max_length=225), blank=True, null=True)  
+    full_description = models.CharField(max_length=1000, blank=True, null=True)
+    email = models.EmailField(unique=True, blank=True, null=True)  
+    other_contact = models.CharField(max_length=225, blank=True, null=True)
+    #discussion = ArrayField(models.CharField(max_length=540), blank=True, null=True)
+    updates = ArrayField(models.CharField(max_length=540), blank=True, null=True)
+    wanted_description = models.CharField(max_length=540, blank=True, null=True)
+    members = models.ForeignKey(
+        'userauth.User',
+        on_delete=models.CASCADE,
+        related_name='member_of_projects',
+        null=True,
+        blank=True
+    )
+    
+    owner = models.ForeignKey(
+        'userauth.User',
+        on_delete=models.CASCADE,
+        related_name='owned_projects',
+        null=True,
+        blank=True
+    )
 
     def __str__(self):
         return self.title
--- backend/userauth/models.py
+++ backend/userauth/models.py
@@ -4,12 +4,27 @@ from django.db import models
 class User(models.Model):
     auth_id = models.IntegerField(default=999)
     username = models.CharField(max_length=255, unique=True)
-    email = models.EmailField(unique=True)  
+    email = models.EmailField(unique=True, blank=True, null=True)  
     institution = models.CharField(max_length=255, blank=True, null=True)
     followers = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
     position = models.JSONField(default=list)
     tags = ArrayField(models.CharField(max_length=225), blank=True, null=True)  
+    
+    projects = models.ForeignKey(
+        'projects.Project', 
+        on_delete=models.CASCADE,
+        related_name='contributors',  # good practice to add related_name
+        null=True,  # add null=True if this can be blank
+        blank=True
+    )
+    owned = models.ForeignKey(
+        'projects.Project',
+        on_delete=models.CASCADE,
+        related_name='owners',
+        null=True,
+        blank=True
+    )
 
     def __str__(self):
         return self.username
