aa12c283cc00f040679fd349b7e1cd515667616e
William Garcia <102125255+ColumnSkunky@users.noreply.github.com>
2025-03-25 21:45:01 -0700

    feat: additions to models (#33)
    * feat: tags to project model
    
    * fix: added tags to migrations
    
    * feat: uuid implementation, email, full description, updates, wanted and links between user and project
    
    * feat: refactor fields
    
    * chore: fix pipeline dual runs
    
    ---------
    
    Co-authored-by: Larry La <larryquocla@gmail.com>

--- backend/userauth/models.py
+++ backend/userauth/models.py
@@ -4,12 +4,25 @@ from django.db import models
 class User(models.Model):
     auth_id = models.IntegerField(default=999)
     username = models.CharField(max_length=255, unique=True)
-    email = models.EmailField(unique=True)  
+    email = models.EmailField(unique=True, blank=True, null=True)  
     institution = models.CharField(max_length=255, blank=True, null=True)
     followers = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
-    position = models.JSONField(default=list)
-    tags = ArrayField(models.CharField(max_length=225), blank=True, null=True)  
+    positions = models.JSONField(default=list)
+    interest_tags = ArrayField(models.CharField(max_length=225), blank=True, null=True)  
+    skill_tags = ArrayField(models.CharField(max_length=225), blank=True, null=True)  
+    location_preference = models.JSONField(default=list)
+
+    projects = models.ManyToManyField(
+        'projects.Project', 
+        related_name='contributors',  
+        blank=True
+    )
+    owned = models.ManyToManyField(
+        'projects.Project',
+        related_name='owners',
+        blank=True
+    )
 
     def __str__(self):
         return self.username
